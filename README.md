0794bf5f13f30d7799a38f88d3d25d03466ad40c
eh-munna <emran.h.munna@gmail.com>
2025-12-01 22:15:47 +0100

    CRUD practiced

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 - Route Handling
 - SQL Querying with PostgreSQL
 - Middleware Integration
-- Not Found Route Handling
+- Not Found Error Handling
 
 ---
 
--- src/server.ts
+++ src/server.ts
@@ -182,6 +182,82 @@ app.post('/todos', async (req: Request, res: Response) => {
   }
 });
 
+app.get('/todos', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+
+    res.status(200).json({
+      success: true,
+      message: 'todos retrieved successfully',
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+// Get single todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM todos WHERE id = $1', [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Failed to fetch todo' });
+  }
+});
+
+// Update todo
+app.put('/todos/:id', async (req, res) => {
+  const { title, completed } = req.body;
+
+  try {
+    const result = await pool.query(
+      'UPDATE todos SET title=$1, completed=$2 WHERE id=$3 RETURNING *',
+      [title, completed, req.params.id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Failed to update todo' });
+  }
+});
+
+// Delete todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const result = await pool.query(
+      'DELETE FROM todos WHERE id=$1 RETURNING *',
+      [req.params.id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    res.json({ success: true, message: 'Todo deleted', data: null });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Failed to delete todo' });
+  }
+});
+
 // Initialize Database Tables
 
 const initDB = async () => {
